3c1303c675f6bd22c0a3131112d5fbc82d4b9b4d
JohnAgudelo <jjdarr@hotmail.com>
2020-06-16 07:55:29 -0500

    clase 3

--- src/main/java/com/sophos/nivel1/Capacitacion/App.java
+++ src/main/java/com/sophos/nivel1/Capacitacion/App.java
@@ -9,5 +9,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int _variable1 = 5;
+        byte nombreByte = 65;
+        System.out.println(nombreByte);
+        short variableShort = Short.MAX_VALUE;
+        System.out.println(variableShort);
+        int variableInt = 78;
+        System.out.println(variableInt);
+        char variableChar = 'j';
+        byte variableCasteoByte = (byte) variableChar;
+        System.out.println(variableCasteoByte);
+        
+        char [] arregloChar = {'J', 'o', 'h', 'n' };
+        for (int i = 0; i < arregloChar.length; i++);
+        int x = (int) arregloChar[i]+1;
+        
+        System.out.println(arregloChar);
     }
+
 }
